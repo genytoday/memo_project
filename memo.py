861796e233a8258de48583ade0d3b4b86c301745
genytoday <geny0822@gmail.com>
2018-09-23 16:00:31 +0900

    improved some files without instance and delete data.py

--- memo.py
+++ memo.py
@@ -1,17 +1,46 @@
-class MemoList:
+import sqlite3
+from contextlib import closing
 
-    def __init__(self,memonaiyou,makeTime,user):
-        self.memonaiyou=memonaiyou
-        self.makeTime=makeTime
-        self.user=user
-
-    def delete(memo_dic,index):
-        del memo_dic[index]
-        return memo_list
+dbname = 'database.db'
 
 
-    def inquiry(memo_dic,index):
-        return memo_dic[index]
+with closing(sqlite3.connect(dbname)) as conn:
+    c = conn.cursor()
 
-    def update(memo_dic,str,index):
-        memo_dic[index] = str
+        # executeメソッドでSQL文を実行する
+    create_table = '''create table memo (id int, user varchar(64),memonaiyou varchar(64),makeTime int)'''
+    c.execute(create_table)
+
+
+def createdata(self, user, memonaiyou, makeTime):
+        # SQL文に値をセットする場合は，Pythonのformatメソッドなどは使わずに，
+        # セットしたい場所に?を記述し，executeメソッドの第2引数に?に当てはめる値を
+        # タプルで渡す．
+    sql = 'insert into memo (id, self.user, self.memonaiyou, self.makeTime) values (?,?,?,?,?)'
+    memo = (user, memonaiyou, makeTime)
+    c.execute(sql, memo)
+
+    select_sql = 'select * from memo'
+    for row in c.execute(select_sql):
+        print(row)
+
+        # 一度に複数のSQL文を実行したいときは，タプルのリストを作成した上で
+        # executemanyメソッドを実行する
+        # insert_sql = 'insert into memos (id, name, age, gender) values (?,?,?,?)'
+        # memos = [
+        #     (2, 'Shota', 54, 'male'),
+        #     (3, 'Nana', 40, 'female'),
+        #     (4, 'Tooru', 78, 'male'),
+        #     (5, 'Saki', 31, 'female')
+        # ]
+        # c.executemany(insert_sql, memos)
+        # conn.commit()
+#def updatedata(self,user,memonaiyou, makeTime):
+
+def inquirydata(self,user,memonaiyou, makeTime):
+    c.execute( "select * from dbname")
+    for row in c:
+        print(str(row[0])+','+row[1])
+    c.close()
+
+#def deliteedata(self,user,memonaiyou, makeTime):
--- memo_bl.py
+++ memo_bl.py
@@ -1,37 +1,33 @@
-from memo import MemoList
+from memo import*
 from datetime import datetime
 
-memo_dic = {}
-count = memo_dic.len()
 
-user = input('名前を入力してください')
+
 while True:
-    menu = {'登録':1,'削除':2,'編集':3,'照会':4}
-    print(menu)
+    user = input('名前を入力してください')
+
+    menus = ['登録:1','削除:2','編集:3','照会:4']
+    for menu in menus:
+        print(menu)
     selected_number = input('メニュー番号を選んでください')
     #メモの追加
     if selected_number == '1':
-        memo = str(input('メモの内容を入力してください'))
-        print(memo)
-        time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-        print(time)
-        memo_dic[str(count)] = MemoList(memo,time,user)
-        count =+1
-        # type(memo_dic)
-        # for key,item in memo_dic:
-        #     print(key,item.value)
+        memonaiyou = str(input('メモの内容を入力してください'))
+        print('メモの内容：'+ memonaiyou)
+        makeTime = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        makeTime = str(makeTime)
+        print('作成日時:'+makeTime)
+        createdata(user,memonaiyou, makeTime)
 
 
     #メモの削除
     elif selected_number == '2':
         index = input('削除したい番号を選んでください:')
-        del memo_dic[str(count)]
         print('メモを削除しました。')
 
     #メモの照会
     elif selected_number == '3':
-        index = input('削除したい番号を選んでください:')
-        memo.inquiry(memo_list,index)
+        memo.inquirydata()
 
     #メモの更新
     elif selected_number == '4':
